da2995903b8c6636915e9c21ac75691698243426
ArthurOReis <arthuroreis2005@gmail.com>
2025-04-03 08:04:38 -0300

    Implementando 'get_triangulo'

--- exercicio-1/app/src/main/java/org/example/App.java
+++ exercicio-1/app/src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ public class App {
                 System.out.println("É um quadrado");
                 break;
             case "triangulo":
-                System.out.println("É um triangulo");
+                get_triangulo(args[1]);
                 break;
             case "losango":
                 System.out.println("É um losango");
@@ -15,6 +15,26 @@ public class App {
             default:
                 System.out.println("Forma desconhecida");
                 break;
+        }
+    }
+    
+    public static void get_triangulo(String tamanho_str){
+        int tamanho = Integer.parseInt(tamanho_str);
+        String triangulo[][] = new String[tamanho][tamanho];
+        int espaco = tamanho - 1;
+
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                triangulo[i][j] = (j - espaco < 0) ? " " : "*";
+            }
+            espaco--;
+        }
+
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                System.out.print(triangulo[i][j]);
+            }
+            System.out.print("\n");
         }
     }
 }
