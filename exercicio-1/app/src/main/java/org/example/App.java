ba74c0e307877555486f8a0ad93963d83b35aa63
ArthurOReis <arthuroreis2005@gmail.com>
2025-04-03 08:41:12 -0300

    Implementando 'get_retangulo' seguido de comentários

--- exercicio-1/app/src/main/java/org/example/App.java
+++ exercicio-1/app/src/main/java/org/example/App.java
@@ -7,8 +7,8 @@ public class App {
         Caso um argumento inválido (não esperado pelo algoritmo), retornará uma resposta padrão seguido de saída;
         */
         switch(args[0]){
-            case "quadrado":
-                System.out.println("É um quadrado");
+            case "retangulo":
+                get_retangulo(args[1], args[2]);
                 break;
             case "triangulo":
                 get_triangulo(args[1]);
@@ -29,7 +29,10 @@ public class App {
 
         /*
         Nessa iteração com um conjunto de vetores do tamanho de um quadrado perfeito, é verificado
-        em cada linha todos os espaços restantes para cada ponto inserido        
+        em cada linha todos os espaços restantes para cada ponto inserido
+        
+        i: altura
+        j: largura
         */
         for (int i = 0; i < tamanho; i++) {
             for (int j = 0; j < tamanho; j++) {
@@ -44,6 +47,38 @@ public class App {
         for (int i = 0; i < tamanho; i++) {
             for (int j = 0; j < tamanho; j++) {
                 System.out.print(triangulo[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void get_retangulo(String largura_str, String altura_str){
+        int largura = Integer.parseInt(largura_str);
+        int altura = Integer.parseInt(altura_str);
+        String retangulo[][] = new String[altura][largura];
+        
+        /*
+        Nessa iteração com um conjunto de vetores do tamanho de um quadrado perfeito, é verificado
+        em cada linha se a iteração está nas extremidades verticais do retângulo; Enquanto que nas
+        extremidades horizontais são preenchidas por padrão
+        
+        i: altura
+        j: largura
+        */
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                retangulo[0][j] = "*";
+                retangulo[i][j] = (j == 0 || j - (largura - 1) == 0) ? "*"  : " ";
+                retangulo[altura - 1][j] = "*";
+            }
+        }
+
+        /*
+        Imprimindo o retângulo
+        */
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                System.out.print(retangulo[i][j]);
             }
             System.out.print("\n");
         }
