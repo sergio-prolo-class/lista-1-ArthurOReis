9efdadfdec0159db8ef531bb9980a78ecb85a4df
ArthurOReis <arthuroreis2005@gmail.com>
2025-04-06 21:57:43 -0300

    Adicionando mais comentários

--- exercicio-3/app/src/main/java/org/example/App.java
+++ exercicio-3/app/src/main/java/org/example/App.java
@@ -1,5 +1,4 @@
 /*
-
 Batalha naval é um jogo de tabuleiro em que dois jogadores precisam afundar a frota do adversário.
 Cada jogador possui um tabuleiro com dimensões 10×10.
 No início da partida, os jogadores posicionam seus navios na vertical ou horizontal, não sendo permitido a sobreposição de navios.
@@ -7,18 +6,16 @@ A Tabela 1 apresenta uma lista dos navios em jogo.
 Desenvolva um aplicativo em Java que gere um tabuleiro, posicione de forma aleatória a frota de um jogador e imprima na tela o tabuleiro gerado.
 Para casas que contém um navio posicionado, imprima o símbolo do navio.
 Para as casas que não contém navios, imprima o caractere ponto. Separe as casas da mesma linha com espaços.
-
 */
 
 /*
-
 Porta-aviões - 5 (Tamanho) - P (Símbolo)
 Encouraçado - 4 (Tamanho) - E (Símbolo)
 Cruzador - 3 (Tamanho) - C (Símbolo)
 Submarino - 3 (Tamanho) - S (Símbolo)
 Contratorpedeiro - 2 (Tamanho) - N (Símbolo)
-
 */
+
 package org.example;
 import java.util.Random;
 
@@ -73,9 +70,7 @@ public class App {
     public static void main(String[] args) {
 
         /*
-        
         Inicializando o tabuleiro em forma de arranjos duplos de String
-        
         */
 
         String[][] tabuleiro = new String[10][10];
@@ -87,12 +82,10 @@ public class App {
         }
 
         /*
-
         Para esse exercício, resolvi criar uma classe do tipo Barco, pois na minha cabeça fazia sentido,
         principalmente pelo fato de que os barcos possuem atributos o suficiente para inserí-los em uma
         estrutura, dessa forma, utilizando a POO a meu favor para facilitar a renderização de barcos no
         tabuleiro, na ordem certa
-        
         */
 
         Barco barcos[] = new Barco[5];
@@ -138,6 +131,13 @@ public class App {
     }
 
     public static void set_Barcos(Barco[] barcos, String[][] tabuleiro) {
+    /*
+    Essa função cria posições para todos os barcos no tabuleiro, sendo o algoritmo responsável por:
+        - Gerar uma posição inicial aleatória (por linha e coluna).
+        - Verificar se a posição e a direção escolhida estão livres com a função 'verificando_posicoes'.
+        - Colocar os barcos no tabuleiro com 'inserir_barco'.
+    E se uma das etapas falhar por acaso, o processo será parado e repetido do começo.
+    */
         Random rand = new Random();
 
         for (int i = 0; i < barcos.length; i++) {
@@ -199,12 +199,6 @@ public class App {
     }
 
     public static void inserir_barco(String[][] tabuleiro, Barco barco, int linha, int coluna, int direcao) {
-
-        /*
-        Após fazer a verificação, será inserido na linha ou coluna o símbolo do barco, obtido através da classe, de acordo com a direção sem
-        nenhuma obstrução.
-        */
-
         /*
         Porta-aviões - 5 (Tamanho) - P (Símbolo)
         Encouraçado - 4 (Tamanho) - E (Símbolo)
